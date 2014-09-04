3e4622fb05d7cf8e230de4d9e924bb85f02b18c5
Lesik <amborsdorfer@gmail.com>
2014-09-04 13:36:40 +0200

    + basic parse functionality

--- src/org/androidfromfrankfurt/archnews/NewsFragment.java
+++ src/org/androidfromfrankfurt/archnews/NewsFragment.java
@@ -1,10 +1,17 @@
 package org.androidfromfrankfurt.archnews;
 
+import java.util.List;
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import at.theengine.android.simple_rss2_android.RSSItem;
+import at.theengine.android.simple_rss2_android.SimpleRss2Parser;
+import at.theengine.android.simple_rss2_android.SimpleRss2ParserCallback;
 
 
 public class NewsFragment extends Fragment {
@@ -15,6 +22,29 @@ public class NewsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+        parseRss();
         return rootView;
+    }
+    
+    private void parseRss() {
+    	SimpleRss2Parser pars = new SimpleRss2Parser("https://www.archlinux.org/feeds/news/",
+    			new SimpleRss2ParserCallback() {
+			
+			@Override
+			public void onFeedParsed(List<RSSItem> arg0) {
+				// TODO Auto-generated method stub
+				for(int i = 0; i < arg0.size(); i++){
+	                Log.d("SimpleRss2ParserDemo",arg0.get(i).getTitle());
+	            }				
+			}
+			
+			@Override
+			public void onError(Exception arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity().getApplicationContext(),
+						arg0.getMessage(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
     }
 }
