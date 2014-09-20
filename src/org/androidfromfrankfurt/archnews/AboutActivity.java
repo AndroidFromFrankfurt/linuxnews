ef05a6804b0d0dc30fb05abbb39e96bed055202a
Lesik <amborsdorfer@gmail.com>
2014-09-20 15:40:27 +0200

    + more convenient way to use the activity "up" button

--- src/org/androidfromfrankfurt/archnews/AboutActivity.java
+++ src/org/androidfromfrankfurt/archnews/AboutActivity.java
@@ -17,23 +17,23 @@ public class AboutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
+//		ActionBar actionBar = getActionBar();
+//		actionBar.setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_about);
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction().add(R.id.container, new AboutFragment()).commit();
 		}
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
-				finish();
-				return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		switch (item.getItemId()) {
+//			case android.R.id.home:
+//				finish();
+//				return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
 	public static class AboutFragment extends PreferenceFragment {
 
 		public AboutFragment() {
