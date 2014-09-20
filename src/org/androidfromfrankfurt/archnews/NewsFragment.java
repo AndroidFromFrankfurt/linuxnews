187f4c55ebfdb026f5a91c62bdeb5f5ca95347be
Lesik <amborsdorfer@gmail.com>
2014-09-20 21:12:00 +0200

    Hide ActionBar on scroll Use HoloAccent library Add Spanish RSS

--- src/org/androidfromfrankfurt/archnews/NewsFragment.java
+++ src/org/androidfromfrankfurt/archnews/NewsFragment.java
@@ -3,8 +3,10 @@ package org.androidfromfrankfurt.archnews;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ListFragment;
@@ -12,17 +14,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnScrollChangedListener;
 import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 import at.theengine.android.simple_rss2_android.RSSItem;
 import at.theengine.android.simple_rss2_android.SimpleRss2Parser;
 import at.theengine.android.simple_rss2_android.SimpleRss2ParserCallback;
 
 
-public class NewsFragment extends ListFragment {
+public class NewsFragment extends ListFragment implements OnScrollListener {
 
 	private static NewsFragment instance;
 	private ListView listView;
@@ -33,6 +38,7 @@ public class NewsFragment extends ListFragment {
 	private ProgressDialog loadingDialog;
 	private boolean listVisible;
 	private boolean newActivity = true;
+	private int mLastFirstVisibleItem;
 	
     public NewsFragment() {
     	instance = this;
@@ -52,6 +58,7 @@ public class NewsFragment extends ListFragment {
 //    	then some WebViews in the list items weirdly change their size after resuming the activity.
 //    	Thus I will just use null to not have this glitch until someone comes up with a better solution.
     	listView.addHeaderView(headerView);
+    	listView.setOnScrollListener(this);
     	tvError = (TextView)rootView.findViewById(R.id.tv_errormessage);
     	btnReload = (Button)rootView.findViewById(R.id.btn_reload);
     	return rootView;
@@ -91,15 +98,23 @@ public class NewsFragment extends ListFragment {
     	System.out.println("GetPref "+sharedPrefs.getInt("lang", 0));
     	int selectedLang = sharedPrefs.getInt("lang", 0);
     	if(selectedLang == 0) {
+    		// English
     		feedUrl = "https://www.archlinux.org/feeds/news/";
     	}
     	else if(selectedLang == 1) {
+    		// German
     		feedUrl = "https://bbs.archlinux.de/extern.php?action=feed&fid=257&type=atom&order=posted&show=15";
     	}
     	else if(selectedLang == 2) {
+    		// French
     		feedUrl = "https://archlinux.fr/feed";
     	}
+    	else if(selectedLang == 3) {
+    		// Spanish
+    		feedUrl = "http://portada.archlinux-es.org/feed";
+    	}
     	else {
+    		// Default (English)
     		feedUrl = "https://www.archlinux.org/feeds/news/";
     	}
     	SimpleRss2Parser newsParser = new SimpleRss2Parser(feedUrl,
@@ -172,4 +187,22 @@ public class NewsFragment extends ListFragment {
             listView.setVisibility(View.VISIBLE);
         }
     }
+    
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		NewsActivity newsActivity = NewsActivity.getThis();
+		ActionBar actionBar = newsActivity.getActionBar();
+		if (mLastFirstVisibleItem < firstVisibleItem && actionBar.isShowing()) {
+            actionBar.hide();
+        }
+
+        if (mLastFirstVisibleItem > firstVisibleItem && !actionBar.isShowing()) {
+            actionBar.show();
+        }
+        mLastFirstVisibleItem = firstVisibleItem;
+	}
 }
