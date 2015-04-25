caccedcf72f6aa947936eb9a4f1603178f297639
Lesik <amborsdorfer@gmail.com>
2015-04-25 21:55:02 +0200

    + add SUSE, Ubuntu, Fedora and generic Tux icons + add icon change in ActionBar - remove color change in ActionBar because it looks bad + add settings icon in ActionBar (no function yet) - remove language icon from ActionBar (will be a setting in the settings later)

---
+++ src/org/androidfromfrankfurt/archnews/SettingsActivity.java
@@ -0,0 +1,13 @@
+package org.androidfromfrankfurt.archnews;
+
+public class SettingsActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
