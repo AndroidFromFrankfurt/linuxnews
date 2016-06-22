9be8df74e12dfd0e914868766f426af02916125a
Lesik <amborsdorfer@gmail.com>
2016-06-22 14:08:22 +0200

    Update translations Monochromatic gnewsense icon

--- app/src/main/java/org/androidfromfrankfurt/archnews/HomeActivity.java
+++ app/src/main/java/org/androidfromfrankfurt/archnews/HomeActivity.java
@@ -48,7 +48,7 @@ public class HomeActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.ui_navigation_drawer_open, R.string.ui_navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
